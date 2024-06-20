91f00adbe2a33b6fb6a40880553b4df0c2d4fe33
Festus <85620487+afari-darko@users.noreply.github.com>
2024-06-20 22:25:27 +0000

    Update main.tf

--- project/terraform/main.tf
+++ project/terraform/main.tf
@@ -76,6 +76,13 @@ resource "aws_security_group" "devops-project-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    // sonarqube
+    description = "sonarqube port"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   egress {
     from_port = 0
     to_port = 0
