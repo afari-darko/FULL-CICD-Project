96cc6817ce433237cf3d94a3d4347458af16c7ac
Festus <85620487+afari-darko@users.noreply.github.com>
2024-07-05 08:53:53 +0000

    Update main.tf

--- project/terraform/main.tf
+++ project/terraform/main.tf
@@ -83,6 +83,7 @@ resource "aws_security_group" "devops-project-sg" {
     to_port     = 9000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+}
   egress {
     from_port = 0
     to_port = 0
